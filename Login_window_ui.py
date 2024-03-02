f154ddeeb6b7b70b1bb5a7eb751cfcd4de34824c
Home <dsshvakin@mail.ru>
2024-03-02 19:31:36 +0300

    дз

--- Login_window_ui.py
+++ Login_window_ui.py
@@ -1,12 +1,6 @@
 from PyQt6.QtWidgets import QLineEdit, QCheckBox
 
-from config import set_config
-from config import read_config
-from Login_window_ui import Ui_MainWindow
 
-import loader
-
-import loader
 
 
 def get_field(ui: Ui_MainWindow) -> list[QLineEdit]:
@@ -22,7 +16,6 @@ def get_checkboxes(ui: Ui_MainWindow) -> list[QCheckBox]:
     ]
 
 
-
 def login_btn_clicked():
     fields = get_field(loader.login_window_ui)
 
@@ -31,12 +24,29 @@ def login_btn_clicked():
     remember_me = get_checkboxes(loader.login_window_ui)[0]
 
     if login.text() == 'Daniil' and password.text() == '12345':
-        pass
+        if remember_me.isChecked():
+            set_config('config.txt', 'login', login.text())
+            set_config('config.txt', 'password', password.text())
+            print('authed with save')
+        else:
+            print('authed without save')
     else:
-        pass
+        print('not ok')
 
 
-def logic(ui: Ui_MainWindow):
+def is_authed() -> bool:
+    cfg = read_config('config.txt')
+
+    return cfg['login'] != 'None'
+
+
+def logic(ui: Ui_MainWindow) -> bool:
     login_btn = ui.login_btn
 
     login_btn.clicked.connect(login_btn_clicked)
+
+    return is_authed()
+
+
+class Ui_MainWindow:
+    pass
---
+++ loader.py
@@ -0,0 +1,3 @@
+from Login_window_ui import Ui_MainWindow
+
+login_window_ui = Ui_MainWindow()
