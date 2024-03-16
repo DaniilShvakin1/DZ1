7be54e0ee4c3f4d4bbda36e96cc0cfcee8206584
Home <dsshvakin@mail.ru>
2024-03-16 18:57:01 +0300

    дз

--- loader.py
+++
@@ -1,3 +0,0 @@
-from Login_window_ui import Ui_MainWindow
-
-login_window_ui = Ui_MainWindow()
--- main.py
+++ main.py
@@ -2,21 +2,11 @@ import sys
 
 from PyQt6.QtWidgets import QApplication, QMainWindow
 
-import loader
-import login_window
+from windows import LoginWindow
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
 
-    # init window
-    login_window_ = QMainWindow()
-    loader.login_window_ui.setupUi(login_window_)
-    # load login window logic
-    if not login_window.logic(loader.login_window_ui):
-        login_window_.show()
-        print('not authed')
-
-    else:
-        print('authed')
+    login_window_ = LoginWindow()
 
     sys.exit(app.exec())
